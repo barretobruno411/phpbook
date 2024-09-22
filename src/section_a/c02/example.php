0f729c8740ad4228c39ac310769d53789ef8da7f
barretobruno411 <bruno.duarte411@gmail.com>
2024-09-22 12:05:01 -0300

    :sparkles: feat: finish chapter 2

--- src/section_a/c02/example.php
+++ src/section_a/c02/example.php
@@ -3,29 +3,29 @@ $name = 'Ivy';                                   // Store the user's name
 
 $greeting = 'Hello';                             // Create initial value for greeting
 if ($name) {                                     // If $name has a value
-    $greeting = 'Welcome back, ' . $name;        // Create personalized greeting
+  $greeting = 'Welcome back, ' . $name;        // Create personalized greeting
 }
 
 $product = 'Lollipop';                           // Product name
-$cost    = 2;                                    // Cost of single pack
+$cost    = 10;                                    // Cost of single pack
 
-for ($i = 1; $i <= 10; $i++) {
-    $subtotal   = $cost * $i;                    // Total for this quantity
-    $discount   = ($subtotal / 100) * ($i * 4);  // Discount for this quantity
-    $totals[$i] = $subtotal - $discount;         // Add discounted price to indexed array
+for ($i = 1; $i <= 20; $i++) {
+  $subtotal   = $cost * $i;                    // Total for this quantity
+  $discount   = ($subtotal / 100) * ($i * 4);  // Discount for this quantity
+  $totals[$i] = $subtotal - $discount;         // Add discounted price to indexed array
 }
 ?>
 
-<?php require 'includes/header.php'; ?> 
+<?php require 'includes/header.php'; ?>
 
-  <p><?= $greeting ?></p>
-  <h2><?= $product ?> Discounts</h2>
-  <table>
-    <tr>
-      <th>Packs</th>
-      <th>Price</th>
-    </tr>
-    <?php foreach ($totals as $quantity => $price) { ?>
+<p><?= $greeting ?></p>
+<h2><?= $product ?> Discounts</h2>
+<table>
+  <tr>
+    <th>Packs</th>
+    <th>Price</th>
+  </tr>
+  <?php foreach ($totals as $quantity => $price) { ?>
     <tr>
       <td>
         <?= $quantity ?>
@@ -35,7 +35,7 @@ for ($i = 1; $i <= 10; $i++) {
         $<?= $price ?>
       </td>
     </tr>
-    <?php } ?>
-  </table>
+  <?php } ?>
+</table>
 
 <?php include 'includes/footer.php' ?>
--- src/section_a/c02/includes/header.php
+++ src/section_a/c02/includes/header.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>The Candy Store</title>
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
-  <body>
-    <h1>The Candy Store</h1>
-    <nav>
-      <a href="index.php">Home</a> | 
-      <a href="candy.php">Candy</a> | 
-      <a href="about.php">About</a> | 
-      <a href="contact.php">Contact</a>
-    </nav>
+
+<head>
+  <title>The Candy Store</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <h1>The Candy Store</h1>
+  <nav>
+    <a href="index.php">Home</a> |
+    <a href="candy.php">Candy</a> |
+    <a href="about.php">About</a> |
+    <a href="contact.php">Contact</a> |
+    <a href="partners.php">Partners</a>
+  </nav>
